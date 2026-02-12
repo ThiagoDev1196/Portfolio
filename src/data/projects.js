eafe8f5a78ddef58057ee74df2899e4f1847a654
Thiago <thiagomarcial1109@gmail.com>
2026-02-12 17:42:19 -0300

    Updating projects pics and desciptions and added CV file for download

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     {
         id: 1,
         name: 'Furina de Fontaine',
-        desc: 'An immersive café landing page with a deep, coffee-inspired palette. Furina de Fontaine allows customers to browse the menu and locate the shop, creating a sensory-driven interface that makes users feel the cozy atmosphere and aroma of Fontaine\'s finest brews.',
+        desc: 'An immersive café landing page with a deep, coffee-inspired palette. Furina de Fontaine allows customers to browse the menu and locate the shop, creating a sensory-driven interface that makes users feel the cozy atmosphere and aroma of Fontaine\'s finest brews. The site has a booking page.',
         img: `${base}projects-pictures/FurinaDeFontaine-preview.png`,
         link: 'https://thiagodev1196.github.io/Furina-de-Fontaine-A/',
         tags: ['Astro', 'HTML', 'CSS', 'JavaScript']
@@ -13,7 +13,7 @@ export const projects = [
     {
         id: 2,
         name: 'Green Evergarden',
-        desc: 'A nature-centric online store for eco-conscious shoppers. Green Evergarden allows users to discover premium sustainable products, browse verified customer feedback, and join a growing community through its integrated subscription system.',
+        desc: 'A nature-centric online store for eco-conscious shoppers. Green Evergarden allows users to discover premium sustainable products, browse verified customer feedback, and join a growing community through its integrated subscription system. The site has a catalog page.',
         img: `${base}projects-pictures/GreenEvergarden-preview.png`,
         link: 'https://thiagodev1196.github.io/Green-Evergarden/',
         tags: ['React', 'Tailwind CSS', 'CSS']
