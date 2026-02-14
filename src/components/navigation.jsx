b8aeff8d25b2718788edc1584ba15a527b22197a
Thiago <thiagomarcial1109@gmail.com>
2026-02-14 16:04:30 -0300

    Updated site color palette and ajusted layout spacing

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -10,7 +10,7 @@ function Header() {
 
     return (
 
-    <nav className="nav__container flex flex-col bg-black items-start md:items-end text-white  fixed  w-full z-30">
+    <nav className="nav__container flex flex-col bg-white/95 text-black items-start md:items-end  fixed  w-full z-30">
 
           <div className="nav__image flex flex-row items-center justify-center  p-4 md:hidden">
            <button className="xl:hidden text-xl " onClick={toggleMenu} id="menu-open" aria-label="Open navegation menu">☰</button>
@@ -20,11 +20,11 @@ function Header() {
 
         <ul className={`nav__ul ${isOpen ? 'flex-col' : 'hidden'} w-full md:flex md:flex-row md:justify-end p-3 md:text-lg`} id="nav">
 
-          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu}  className="hover:text-gray-400 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#">Home</a></li>
-          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-400 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#projects">Projects</a></li>
-          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-400 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#skills">Skills</a></li>
-          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-400 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#about">About me</a></li>
-          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-400 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#contact">Contact me</a></li>
+          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu}  className="hover:text-gray-500 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#">Home</a></li>
+          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-500 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#projects">Projects</a></li>
+          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-500 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#skills">Skills</a></li>
+          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-500 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#about">About me</a></li>
+          <li className="p-6 pl-4 md:p-4 font-semibold"><a onClick={toggleMenu} className="hover:text-gray-500 transition ease-in lg:px-6 lg:py-2 lg:rounded-3xl" href="#contact">Contact me</a></li>
         </ul>     
     </nav>
 
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -28,29 +28,29 @@ const currentProject = projects[index];
 
 return (
 
-    <section id="projects" className="max-w-425 m-auto scroll-mt-15 md:scroll-mt-26">
+    <section id="projects" className="max-w-425 m-auto scroll-mt-15 py-10 md:scroll-mt-20 bg-white/95">
 
-    <h2 className="text-3xl font-bold py-4 text-white text-center md:text-4xl lg:text-5xl lg:mb-8">Projects</h2>
+    <h2 className="text-3xl font-bold py-4 text-black text-center md:text-4xl lg:text-5xl lg:mb-8">Projects</h2>
 
-    <div className="projects-container flex flex-col text-white justify-center items-center p-2 h-160 md:flex-row-reverse md:h-auto md:p-4 lg:h-full">
+    <div className="projects-container flex flex-col text-black justify-center items-center p-2 h-160 md:flex-row-reverse md:h-auto md:p-4 lg:h-full">
 
-    <button onClick={handleNext} className="button-md hidden md:flex text-gray-500 px-6 text-3xl cursor-pointer hover:text-white lg:text-5xl"> › </button>
+    <button onClick={handleNext} className="button-md hidden md:flex text-gray-800 px-6 text-3xl cursor-pointer hover:text-gray-500 lg:text-5xl"> › </button>
 
         <div className="project-text-container flex flex-col justify-center items-center space-y-4 p-2 md:px-5">
             <h3 className="text-lg font-semibold md:text-2xl">{currentProject.name} </h3>
             <p className="text-sm text-center md:text-start md:h-50 md:flex md:justify-center md:items-center md:text-md lg:text-lg">{currentProject.desc}</p>
-            <a href={currentProject.link} target="_blank" className="bg-white text-black px-4 py-2 text-sm md:text-md lg:text-lg lg:px-6 rounded-lg hover:bg-gray-200">Visit the page</a>
+            <a href={currentProject.link} target="_blank" className="bg-purple-700 text-white px-4 py-2 text-sm md:text-md lg:text-lg lg:px-6 rounded-lg hover:bg-purple-900">Visit the page</a>
         </div>
         <div className="project-img-container max-w-120 pt-4 md:min-w-100 md:max-w-160">
             <img className="w-full h-full " src={currentProject.img} alt="project picture preview" />
         </div>
 
         <div className="project-button-container space-x-3 mt-auto mb-36 md:hidden">
-            <button onClick={handlePrev} className="bg-white text-sm text-black px-4 py-2 rounded-lg hover:bg-gray-200">Previous</button>
-            <button onClick={handleNext} className="bg-white text-sm text-black px-4 py-2 rounded-lg hover:bg-gray-200">Next</button>
+            <button onClick={handlePrev} className="bg-purple-600 text-sm text-white px-4 py-2 rounded-lg hover:bg-purple-800">Previous</button>
+            <button onClick={handleNext} className="bg-purple-600 text-sm text-white px-4 py-2 rounded-lg hover:bg-purple-800">Next</button>
         </div>
     
-    <button onClick={handlePrev} className="button-md hidden md:flex text-gray-500 px-6 text-3xl cursor-pointer hover:text-white lg:text-5xl">‹</button>
+    <button onClick={handlePrev} className="button-md hidden md:flex text-gray-800 px-6 text-3xl cursor-pointer hover:text-gray-500 lg:text-5xl">‹</button>
 
     </div>
     </section>
